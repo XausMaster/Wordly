c418f0e11cfbf755ae5971d1d9fb3dcd1d8e4e7d
XausMaster <ibrahimisazade264@gmail.com>
2023-07-18 10:45:29 +0400

    Update 0.1.1.2 ( Small Update ) * DataBase POST Func * Integration TypeScript

--- Client/app/pages/index.tsx
+++ Client/app/pages/index.tsx
@@ -5,6 +5,10 @@ export default function HomePage() {
   const users_url: string = 'http://127.0.0.1:5000/users'
   const [users, setUsers] = useState('')
 
+  const [word, setWord] = useState('')
+  const [difficult, setDifficult] = useState('')
+  const [lcount, setLcount] = useState('')
+
   useEffect(() => {
     console.log("Fetching data...");
     const fetchData = async () => {
@@ -23,13 +27,36 @@ export default function HomePage() {
     fetchData()
   }, [])
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const word_obj = { word, difficult, letter_count: parseInt(lcount) };
+  
+    fetch('http://127.0.0.1:5000/addWord', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(word_obj)
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to add word');
+        }
+        console.log('I sent a new word');
+        // You can add any further logic or state updates here if needed
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error, e.g., display an error message to the user
+      });
+  };
+  
+
   return (
     <>
       <main className="p-5">
-        <form action="" method="post">
-          <input type="text" placeholder="Word" />
-          <input type="text" placeholder="Difficult" />
-          <input type="text" placeholder="Letter Count" />
+        <form onSubmit={handleSubmit} method="post">
+          <input onChange={(e) => {setWord(e.target.value)}} type="text" name="word" placeholder="Word" />
+          <input onChange={(e) => {setDifficult(e.target.value)}} type="text" name="difficult" placeholder="Difficult" />
+          <input onChange={(e) => {setLcount(e.target.value)}} type="text" name="letter_count" placeholder="Letter Count" />
           <input type="submit" value="Send" />
         </form>
       </main>
